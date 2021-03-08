f14a99ab2624d878d0787eaeda15a3f2f49fae3a
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-03-08 12:43:25 +0200

    initial commit

---
+++ o1/index.php
@@ -0,0 +1,33 @@
+<?php
+
+include __DIR__.'/Bebras.php';
+include __DIR__.'/Udra.php';
+
+
+$obj1 = new Bebras;
+$obj2 = $obj1;
+
+// $obj2 = new Bebras;
+
+$obj3 = new Udra;
+
+
+
+echo '<pre>';
+
+
+
+// echo $obj1->tail;
+// echo $obj1->age;
+
+$obj1->sayTail();
+$obj1->getAge();
+
+$obj1->tail = 'SMALL';
+
+echo '<br>';
+var_dump($obj1);
+var_dump($obj2);
+var_dump($obj3);
+
+
