7e17f4bdec0f4edb2725965844f51340cef288a4
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-03-09 12:43:54 +0200

    update Bebras

--- o1/Bebras.php
+++ o1/Bebras.php
@@ -4,19 +4,107 @@ class Bebras
 {
 
     public $tail = 'BIG';
-    private $age = 23;
+    private $age = 15;
+
+    public static $legs = 2;
+
+    public $prop = 'BLABLA';
+
+    private $wife = 'blod', $children1 = 'beaver1', $children2 = 'beaver3', $dog = 'bobik';
+
+    private static $BebroObjektas;
+
+    static public function createBebras($age)
+    {
+        echo 'Lets create';
+
+        
+
+
+        return self::$BebroObjektas ?? self::$BebroObjektas = new self($age);
+    }
+
+
+    private function __construct($age)
+    {
+        echo '<br>';
+        echo 'Konstruojam';
+        $this->udra = new Udra; // <--- dinamiskai sukurta savybe (vienos klases viduje panaudotos kitos klases savybes) (pries naudojant visos klases turi buti uzloadintos)
+        self::$legs = 85;
+
+        echo self::$legs;
+
+        echo '<br>';
+        if (is_integer($age)) {
+            $this->init1($age);
+        }
+        if (is_string($age)) {
+            $this->init2($age);
+        }
+
+        // $this->age = $age;
+    }
+
+    public function init1($age)
+    {
+        echo '<br>';
+        echo 'INIT1';
+        echo '<br>';
+        $this->age = $age;
+    }
+
+    public function init2($age)
+    {
+        echo '<br>';
+        echo 'INIT2';
+        echo '<br>';
+        $this->wife = $age;
+    }
+
+
+    public function __get($prop)
+    {
+        if (!in_array($prop, ['wife', 'children1', 'age'])) {
+            return;
+        }
+
+        return $this->$prop;
+    }
+
+
+    public function __set($prop, $value)
+    {
+        if ('age' == $prop && $value > 30) {
+            echo 'Blogi metai';
+            return;
+        }
+
+        $this->$prop = $value;
+    }
+
 
     public function sayTail()
     {
         echo '<br>';
-        echo 'Hello '.$this->tail;
+        echo 'Hello ' . $this->tail;
         echo '<br>';
     }
 
-    public function getAge() //<--- geteris
+
+    public function getAge() //<----- geteris
     {
         echo '<br>';
-        echo 'Age is: '.$this->age;
+        echo 'Age is: ' . rand(1, 18);
         echo '<br>';
+    }
+
+    public function setAge($age) //<----- seteris
+    {
+        if ($age > 30) {
+            echo 'Blogi metai';
+            return;
+        }
+
+        $this->age = $age;
     }
 }
--- o1/index.php
+++ o1/index.php
@@ -1,13 +1,19 @@
 <?php
 
-include __DIR__.'/Bebras.php';
-include __DIR__.'/Udra.php';
 
+include __DIR__ .'/Bebras.php';
+include __DIR__ .'/Udra.php';
 
-$obj1 = new Bebras;
+// Bebras::$legs = 55;
+// Bebras::createBebras(45);
+$obj1 = Bebras::createBebras(19);
+
+// $obj1->init(77);
+
 $obj2 = $obj1;
 
-// $obj2 = new Bebras;
+$obj2 = Bebras::createBebras(16);
+
 
 $obj3 = new Udra;
 
@@ -17,13 +23,28 @@ echo '<pre>';
 
 
 
-// echo $obj1->tail;
 // echo $obj1->age;
 
-$obj1->sayTail();
+$obj1->setAge(88);
+
 $obj1->getAge();
 
+
 $obj1->tail = 'SMALL';
+echo '<br>';
+
+
+
+
+// echo '<br>';
+
+// $obj1->age = 1000;
+
+// echo $obj1->children1;
+// echo $obj1->age;
+
+echo Bebras::$legs = 55;
+
 
 echo '<br>';
 var_dump($obj1);
