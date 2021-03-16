b47af709b72e4bd36141ed6271027930afcb0af0
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-03-16 11:16:15 +0200

    format fix

--- nd_Arvydo/nd3/Krepsys.php
+++ nd_Arvydo/nd3/Krepsys.php
@@ -5,7 +5,7 @@ class Krepsys {
     private $yra = 0;
     const TALPA = 500;
 
-    public function deti(Grybas $grybas)
+    public function deti(Grybas $grybas) : bool
     {
         if($grybas->valgomas && !$grybas->sukirmijes) {
             $this->yra = $this->yra + $grybas->svoris;
