b05cad5e6640c3d956021fd73bb29400ce608193
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-03-17 13:49:13 +0200

    update

---
+++ nd_Arvydo/nd3/Miskas/Didelis/Krepsys.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Miskas\Didelis;
+use Miskas\Mazas\Grybas;
+
+class Krepsys {
+
+    private $yra = 0;
+    const TALPA = 500;
+
+    public function deti(Grybas $grybas) : bool
+    {
+        if($grybas->valgomas && !$grybas->sukirmijes) {
+            $this->yra = $this->yra + $grybas->svoris;
+        }
+        return self::TALPA >= $this->yra;
+    }
+
+}
